1e950ba9978509626beced6b6381871b5d3bc68f
JMcB#7918 <joel.mcbride1@live.com>
2021-01-18 19:10:16 +0000

    Add command-line interface

--- auto_timelapse.py
+++ auto_timelapse.py
@@ -23,6 +23,7 @@ Options (variables at start of file, better options system to follow):
 import re
 import os
 import sys
+import argparse
 import concurrent.futures
 
 import youtube_dl as youtube_yl  # youtube yownloader
@@ -34,33 +35,38 @@ __version__ = '0.6.8'
 
 # TODO: command-line argument support
 # TODO: - vod urls as a list of arguments
-# TODO: - prefer 1080p option (720p default)
-# TODO: - delete sped up vods and only keep concat version
-# TODO: - option to overwrite output folder
-# TODO: - option for how much to speed up video
-# TODO: - option to keep audio in timelapse?
 # TODO: - option to turn off logging for youtube_dl and ffmpeg
 # TODO: handle vod list input through input() after running
 # TODO: option to disable multithreading or use a batch mode for large lists
-# TODO: refine function arguments, reduce use of 'constants'
 # TODO: ffmpeg --enable-nvenc option
 # TODO: function to get list of videos from option like youtube channel, maybe with time conditions
 
 
 YOUTUBE_DL_DEFAULT_OUTTMPL = '%(title)s-%(id)s.%(ext)s'
 
-vods_list_file_path = 'vods.txt'
-out_folder = 'downloads'
-clear_out_folder = False
-prefer_best_quality = False
+
+# command line interface
+parser = argparse.ArgumentParser(description='Script for automatically downloading a list of videos, speeding them up, '
+                                             'and concatenating them.')
+parser.add_argument('--version', action='version', version=__version__)
+
+parser.add_argument('urls', nargs='*', default=[])
+
+parser.add_argument('-f', '--file', default='vods.txt', dest='vods_list_file_path')
+parser.add_argument('-of', '--out-folder', '--output', default='downloads', dest='out_folder')
+parser.add_argument('--overwrite', action='store_true', dest='clear_out_folder')
+parser.add_argument('-b', '--prefer-best-quality', action='store_true', dest='prefer_best_quality')
+parser.add_argument('-s', '--speed', type=int, default=1000, dest='speed')
+parser.add_argument('-n', '--output-timelapse-name', default='_timelapse.mp4', dest='output_timelapse_filename')
+parser.add_argument('-k', '--keep-timelapse-parts', action='store_true', dest='keep_timelapse_parts')
+
+args = parser.parse_args()
+
 # TODO: make this option work
 # keep_full_length_versions = False
-speed = 1000
-output_timelapse_filename = '_timelapse.mp4'
-keep_timelapse_parts = True
 
 
-def out_folder_empty(overwrite=clear_out_folder):
+def out_folder_empty(out_folder=args.out_folder, overwrite=args.clear_out_folder):
     """Check whether the output folder is empty, and clear it if applicable.
 
     Args:
@@ -101,7 +107,7 @@ def remove_duplicates(videos):
     return unique_videos
 
 
-def vods_list_from_file(path=vods_list_file_path):
+def vods_list_from_file(path=args.vods_list_file_path):
     """Get a list of video URL's from a file.
 
     Prints an error message and exits the program if the file is not found.
@@ -121,7 +127,7 @@ def vods_list_from_file(path=vods_list_file_path):
         return vods_list
 
 
-def download(vods_list):
+def download(vods_list, out_folder=args.out_folder, prefer_best_quality=args.prefer_best_quality):
     """Download videos from a list of URL's using youtube-dl, and speed them up using ffmpeg.
 
     Downloads videos then automatically invokes the speed_up function on their filepath.
@@ -149,7 +155,7 @@ def download(vods_list):
         print(f'Invalid video URL, skipping. Invalid URL: {invalid_url}')
 
 
-def speed_up(video_download):
+def speed_up(video_download, speed=args.speed):
     """Speeds up a video using ffmpeg. Works as a youtube_dl progress hook.
 
     The original video will be deleted. The speed multiplier is the 'speed' constant.
@@ -170,7 +176,8 @@ def speed_up(video_download):
     os.remove(video_download['filename'])
 
 
-def combine_videos_in(folder=out_folder):
+def combine_videos_in(folder=args.out_folder,
+                      out_filename=args.output_timelapse_filename, keep_parts=args.keep_timelapse_parts):
     """Combine videos in a folder into a single video using the ffmpeg concatenation demuxer.
 
     Original videos will be removed if keep_timelapse_parts is False.
@@ -185,10 +192,10 @@ def combine_videos_in(folder=out_folder):
         parts_file.writelines([f"file '{os.path.join(folder, video)}'\n" for video in videos])
 
     stream = ffmpeg.input(parts_file_path, format='concat', safe=0)
-    stream = ffmpeg.output(stream, os.path.join(folder, output_timelapse_filename), c='copy')
+    stream = ffmpeg.output(stream, os.path.join(folder, out_filename), c='copy')
     ffmpeg.run(stream)
 
-    if not keep_timelapse_parts:
+    if not keep_parts:
         os.remove(parts_file_path)
         for video in videos:
             os.remove(os.path.join(folder, video))
@@ -202,7 +209,7 @@ def main():
     Finally combines the downloaded videos using the combine_videos_in function.
     """
     if not out_folder_empty():
-        print(f"The output folder '{out_folder}' contains files, please clear it or change the output folder.")
+        print(f"The output folder '{args.out_folder}' contains files, please clear it or change the output folder.")
         sys.exit()
 
     vods_list = vods_list_from_file()
@@ -210,7 +217,7 @@ def main():
     with concurrent.futures.ThreadPoolExecutor() as threads:
         threads.map(download, [[vod] for vod in vods_list])
     print('Downloading finished, combining videos.')
-    combine_videos_in(out_folder)
+    combine_videos_in(args.out_folder)
 
     print('Done.')
 
